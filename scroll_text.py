69d2bb4d9ba76ebf780de4f6821bc6c6bc8a8147
Dayne Broderson <broderson@gmail.com>
2018-02-23 23:38:18 -0800

    adding gracefull exit to scroll_text.py

--- scroll_text.py
+++ scroll_text.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 import sys 
+import signal
 from sense_hat import SenseHat
 sense = SenseHat()
+
+def exit_gracefully(signal, frame):
+    sense.clear()
+    sys.exit(0)
+signal.signal(signal.SIGINT, exit_gracefully)
+
 argv = sys.argv
 program_name = argv.pop(0)
 argv_string = ' '.join(argv)
